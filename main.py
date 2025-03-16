64a5862e7c7ff17809f0e67706503fcba602e93c
Лев Александрович <levucy0@gmail.com>
2025-03-16 19:58:57 +0300

    v1.1

--- main.py
+++ main.py
@@ -3,18 +3,19 @@ import pygame
 
 
 def main():
-    global width, height, screen
+    global width, height, screen, arrowpos
     pygame.init()
     size = width, height = 1200, 800
     screen = pygame.display.set_mode(size)
     pygame.display.set_caption('yandexmaps v1')
     running = True
+    arrowpos = -1
 
     server_address = 'http://geocode-maps.yandex.ru/1.x/?'
     api_key = '40d1649f-0493-4b70-98ba-98533de7710b'
 
-    arrowcoords = (102, 230)
-    coordinates = 0.100001, 0.100001
+    arrowcoords = (100, 230)
+    coordinates = "00.000000, 000.000000"
 
     texts(arrowcoords, coordinates)
     pygame.display.flip()
@@ -26,8 +27,10 @@ def main():
                 running = False
             if event.type == pygame.KEYDOWN:
                 arrowcoords, coordinates = coordinates_change(event.key, list(arrowcoords), coordinates)
+                texts(arrowcoords, coordinates)
             if event.type == pygame.MOUSEBUTTONDOWN:
-                maprender()
+                coordinates = button_click(event.pos, coordinates)
+                texts(arrowcoords, coordinates)
 
         pygame.display.flip()
 
@@ -37,13 +40,15 @@ def main():
 
 
 def texts(arrowcoords, coordinates):
+    screen.fill((0, 0, 0))
+
     font = pygame.font.Font(None, 70)
     text = font.render("впишите координаты", True, (200, 200, 200))
     text_x, text_y = 100, 100
     screen.blit(text, (text_x, text_y))
 
     font = pygame.font.Font(None, 50)
-    text = font.render(str(coordinates)[1:-1], True, (200, 200, 200))
+    text = font.render(coordinates, True, (200, 200, 200))
     text_x, text_y = 100, 200
     screen.blit(text, (text_x, text_y))
 
@@ -57,26 +62,47 @@ def texts(arrowcoords, coordinates):
     text_x, text_y = 100, 700
     screen.blit(text, (text_x, text_y))
 
+    font = pygame.font.Font(None, 50)
+    texts = ["-", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    for i in range(len(texts)):
+        text = font.render(texts[i], True, (100, 200, 100))
+        text_x, text_y = 100 + 45 * i, 300
+        screen.blit(text, (text_x, text_y))
+        text_w = text.get_width()
+        text_h = text.get_height()
+        pygame.draw.rect(screen, (100, 200, 100), (text_x + text_w // 2 - 20, text_y + text_h // 2 - 20, 40, 40), 1)
+
 
 def coordinates_change(key, arrowcoords, coordinates):
+    global arrowpos
+    poslist = [100, 125, 150, 170, 190, 210, 235, 255, 290, 315, 335, 360, 385, 405, 425, 445, 465]
+    arrowpos = poslist.index(arrowcoords[0])
     b = coordinates
-    screen.fill((0, 0, 0))
-    if key == 1073741904 and arrowcoords[0] != 102:
-        arrowcoords[0] -= 20
-        if list(coordinates)[(arrowcoords[0] - 100) // 20] == '.':
-            pass
-            # arrowcoords[0] -= 5
-    if key == 1073741903 and arrowcoords[0] != 482:
-        arrowcoords[0] += 20
-    if key == 1073741906:
-        pass
-    if key == 1073741905:
-        pass
+    if key == 1073741904 and poslist.index(arrowcoords[0]) != 0:
+        arrowcoords[0] = poslist[poslist.index(arrowcoords[0]) - 1] # -= 20
+    if key == 1073741903 and poslist.index(arrowcoords[0]) != len(poslist) - 1:
+        arrowcoords[0] = poslist[poslist.index(arrowcoords[0]) + 1] # += 20
     texts(arrowcoords, b)
     return tuple(arrowcoords), b
 
 
-def maprender():
+def button_click(pos, coordinates):
+    button = (pos[0] - 85) // 45
+    if button >= 0 and button <= 10:
+        coordinates = coordinates.replace(", ", "")
+        coordinates = coordinates.replace(".", "")
+        coordinates = list(coordinates)
+        if button != 0:
+            coordinates[arrowpos] = str(button - 1)
+        else:
+            pass
+        coordinates = ''.join(coordinates)
+        coordinates0 = coordinates[:1] + "." + coordinates[1:7] + ", " + coordinates[7:9] + "." + coordinates[9:]
+        return coordinates0
+    return coordinates
+
+
+def map_render():
     pass
 
 
