a3a2235ea1f5282689da72945a6bf4f6ddd22bc3
Лев Александрович <levucy0@gmail.com>
2025-03-18 14:57:44 +0300

    v1.2

--- main.py
+++ main.py
@@ -3,21 +3,17 @@ import pygame
 
 
 def main():
-    global width, height, screen, arrowpos
+    global width, height, screen
     pygame.init()
     size = width, height = 1200, 800
     screen = pygame.display.set_mode(size)
-    pygame.display.set_caption('yandexmaps v1')
+    pygame.display.set_caption('yandexmaps v1.2')
     running = True
-    arrowpos = -1
+    textpos = 0
 
-    server_address = 'http://geocode-maps.yandex.ru/1.x/?'
-    api_key = '40d1649f-0493-4b70-98ba-98533de7710b'
+    coordinates = "__.______, ___.______"
 
-    arrowcoords = (100, 230)
-    coordinates = "00.000000, 000.000000"
-
-    texts(arrowcoords, coordinates)
+    texts(coordinates)
     pygame.display.flip()
 
     while running:
@@ -25,12 +21,9 @@ def main():
 
             if event.type == pygame.QUIT:
                 running = False
-            if event.type == pygame.KEYDOWN:
-                arrowcoords, coordinates = coordinates_change(event.key, list(arrowcoords), coordinates)
-                texts(arrowcoords, coordinates)
             if event.type == pygame.MOUSEBUTTONDOWN:
-                coordinates = button_click(event.pos, coordinates)
-                texts(arrowcoords, coordinates)
+                coordinates, textpos = button_click(event.pos, coordinates, textpos)
+                texts(coordinates)
 
         pygame.display.flip()
 
@@ -39,7 +32,7 @@ def main():
     pygame.quit()
 
 
-def texts(arrowcoords, coordinates):
+def texts(coordinates):
     screen.fill((0, 0, 0))
 
     font = pygame.font.Font(None, 70)
@@ -50,11 +43,6 @@ def texts(arrowcoords, coordinates):
     font = pygame.font.Font(None, 50)
     text = font.render(coordinates, True, (200, 200, 200))
     text_x, text_y = 100, 200
-    screen.blit(text, (text_x, text_y))
-
-    font = pygame.font.Font(None, 40)
-    text = font.render("^", True, (255, 50, 50))
-    text_x, text_y = arrowcoords
     screen.blit(text, (text_x, text_y))
 
     font = pygame.font.Font(None, 70)
@@ -73,37 +61,31 @@ def texts(arrowcoords, coordinates):
         pygame.draw.rect(screen, (100, 200, 100), (text_x + text_w // 2 - 20, text_y + text_h // 2 - 20, 40, 40), 1)
 
 
-def coordinates_change(key, arrowcoords, coordinates):
-    global arrowpos
-    poslist = [100, 125, 150, 170, 190, 210, 235, 255, 290, 315, 335, 360, 385, 405, 425, 445, 465]
-    arrowpos = poslist.index(arrowcoords[0])
-    b = coordinates
-    if key == 1073741904 and poslist.index(arrowcoords[0]) != 0:
-        arrowcoords[0] = poslist[poslist.index(arrowcoords[0]) - 1] # -= 20
-    if key == 1073741903 and poslist.index(arrowcoords[0]) != len(poslist) - 1:
-        arrowcoords[0] = poslist[poslist.index(arrowcoords[0]) + 1] # += 20
-    texts(arrowcoords, b)
-    return tuple(arrowcoords), b
-
-
-def button_click(pos, coordinates):
+def button_click(pos, coordinates, textpos):
+    coordinates0 = coordinates
     button = (pos[0] - 85) // 45
-    if button >= 0 and button <= 10:
+    if 0 <= button <= 10 and 300 <= pos[1] <= 340:
         coordinates = coordinates.replace(", ", "")
         coordinates = coordinates.replace(".", "")
         coordinates = list(coordinates)
         if button != 0:
-            coordinates[arrowpos] = str(button - 1)
+            coordinates[textpos] = str(button - 1)
+            if textpos != 16:
+                textpos += 1
         else:
             pass
         coordinates = ''.join(coordinates)
-        coordinates0 = coordinates[:1] + "." + coordinates[1:7] + ", " + coordinates[7:9] + "." + coordinates[9:]
-        return coordinates0
-    return coordinates
+        coordinates0 = coordinates[:2] + "." + coordinates[2:8] + ", " + coordinates[8:11] + "." + coordinates[11:]
+    return coordinates0, textpos
 
 
-def map_render():
-    pass
+def map_render(coordinates):
+    server_address = 'https://static-maps.yandex.ru/v1'
+    # server_address = 'http://geocode-maps.yandex.ru/1.x/?'
+    api_key = '40d1649f-0493-4b70-98ba-98533de7710b'
+    spn = 0.05, 0.03
+    geocoder_request = f'{server_address}ll={coordinates}&spn={spn}apikey={api_key}'
+    response = requests.get(geocoder_request)
 
 
 if __name__ == "__main__":
